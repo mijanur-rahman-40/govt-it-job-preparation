7a0094a5c1ffd2b62f473f6bf00db8b493c113b9
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2024-05-07 20:10:18 +0600

    Updated...

---
+++ MYSQL/Practices/Home_WORK.sql
@@ -0,0 +1,80 @@
+use sql_store;
+
+-- Using WHERE 
+SELECT *
+FROM order_items
+WHERE order_id = 6 AND unit_price * quantity > 30;
+
+-- Get the customers whose  :
+-- addresses contain TRAIL or AVENUE
+SELECT *
+FROM customers
+WHERE address LIKE '%trail%' OR address LIKE '%avenue%';
+
+-- Phone numbers end with 9
+SELECT *
+FROM customers
+WHERE phone LIKE '%9';
+
+-- Phone numbers not end with 9
+SELECT *
+FROM customers
+WHERE phone NOT LIKE '%9';
+
+-- REGEXP PRACTICE
+-- GET the customers
+
+-- Returns customers whose first name ends with EY or ON 
+SELECT *
+FROM customers
+WHERE last_name REGEXP 'EY$|ON$';
+-- or 
+-- WHERE last_name REGEXP '(EY|ON)$'
+
+-- Returns customers whose first name starts with MY or contains SE
+SELECT *
+FROM customers
+WHERE last_name REGEXP '^MY|SE';
+
+-- Returns customers whose first name contains B followed by R or U
+SELECT *
+FROM customers
+WHERE last_name REGEXP 'b[ru]';
+-- or 
+-- WHERE last_name REGEXP 'br|bu'
+
+-- Get the order that are not shipped
+SELECT *
+FROM orders 
+WHERE shipped_date IS NULL OR shipper_id IS NULL;
+
+
+-- order by total price
+SELECT * 
+FROM order_items
+WHERE order_id = 2
+ORDER BY quantity * unit_price DESC;
+
+-- using alise for better understanding
+SELECT *, quantity * unit_price as total_price 
+FROM order_items
+WHERE order_id = 2
+ORDER BY quantity * unit_price DESC;
+
+-- more minimizing
+SELECT *, quantity * unit_price as total_price 
+FROM order_items
+WHERE order_id = 2
+ORDER BY total_price DESC;
+
+-- get the top three loyal customer
+SELECT *
+FROM customers
+ORDER BY points DESC
+LIMIT 3;
+
+SELECT *
+FROM customers
+WHERE birth_date > '1990-01-01'
+ORDER BY points DESC
+LIMIT 3;
--- MYSQL/Practices/SQL_BASIC.sql
+++ MYSQL/Practices/SQL_BASIC.sql
@@ -1,2 +1,17 @@
--- Query performing on Mosh Course database : sql_hr, sql_inventory, sql_invooicing, sql_store
-USE sql_store;
+
+DROP DATABASE IF EXISTS `sql_basic`;
+CREATE DATABASE `sql_basic`; 
+USE `sql_basic`;  
+
+CREATE TABLE `user`(
+  `user_id` TINYINT(4) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `address` VARCHAR(10000),
+  PRIMARY KEY(`user_id`)
+) ENGINE=INNODB AUTO_INCREMENT=5 DEFAULT CHARSET=UTF8MB4 COLLATE=UTF8MB4_0900_AI_CI;
+
+-- Insering new data to user table -- 
+INSERT INTO `user` VALUES(1, 'Mijanur Rahman', '');
+INSERT INTO `user` VALUES(2, 'HASAN Rahman', 'Sylhet');
+
+SELECT * FROM `sql_basic`.`user` 
