484ed3c0a76cdc01adf770f5a8b05a21fe33998b
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2024-08-14 20:20:06 +0600

    Updated....

--- C_AND_C++/MODIFY OPERAORS/1.c
+++ C_AND_C++/MODIFY OPERAORS/1.c
@@ -4,7 +4,10 @@ int main(){
     int a = 4, b = 5;
 
     b = a++ + a--;
+    printf("b = %d\n", b);
     a = ++b + b--;
+    // please describe the output of the above line
+
 
     printf("a = %d, b = %d\n", a, b);
 }
